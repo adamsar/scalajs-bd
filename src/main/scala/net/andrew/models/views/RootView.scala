33c862d52f34cb49595992311c2d19480f357f11
Andrew Adams <adamsar@gmail.com>
2017-03-12 21:21:17 +0900

    Styles and initial rendering

--- src/main/scala/net/andrew/models/views/RootView.scala
+++ src/main/scala/net/andrew/models/views/RootView.scala
@@ -1,24 +1,43 @@
 package net.andrew.models.views
 
 import diode.ModelRO
-import net.andrew.models.game.{GamePhase, GameState, Round}
+import net.andrew.models.game._
+import net.andrew.styles.Global
 
 import scalatags.JsDom.all._
 import scalatags.jsdom.Frag
 
 case class RootView(rootModel: ModelRO[GameState]) extends View {
+  import Global.mdl
 
   def viewForPhase: GamePhase[_ <: Round] => View = {
     case _ => RootView(rootModel)
   }
 
-  /**
-    *
-    * @return
-    */
+  def renderHeader: Frag = {
+    header(`class` := mdl.header,
+      div(`class` := mdl.headerRow,
+        span(`class` := mdl.title, "BLAZING DUEEEEEELS")))
+  }
+
+  def renderPullOut: Frag = {
+    div(`class` := mdl.drawer,
+      span(`class` := mdl.title, "Test"),
+      tag("nav")(attr("class") := mdl.nav.container)("test"))
+  }
+
+  def renderContent: Frag = rootModel().round match {
+    case Setup => div("setup")
+    case PlayerRound(round) => div(round)
+    case GameOver => div("game over")
+  }
+
   override def render: Frag = {
-    div(`class` := "container")(
-      "test this"
+    div(`class` := mdl.layout,
+      renderHeader,
+      renderPullOut,
+      div(`class` := mdl.content,
+        renderContent)
     )
   }
 
--- src/main/scala/net/andrew/styles/Global.scala
+++ src/main/scala/net/andrew/styles/Global.scala
@@ -7,16 +7,36 @@ import scalacss.Defaults._
 object Global extends StyleSheet.Standalone {
   import dsl._
 
+  "body" - (
+    margin(0 px),
+    padding(0 px)
+  )
+
   object mdl {
 
     def get(classNames: String*): String = {
-      classNames map (c => s"-$c") mkString " "
+      classNames map (c => s"mdl-$c") mkString " "
     }
 
 
     val layout = get("layout", "js-layout", "layout--fixed-header")
     val header = get("layout__header")
     val headerRow = get("layout__header-row")
+    val title = get("layout-title")
+    val spacer = get("layout-spacer")
+    val drawer = get("layout__drawer")
+    val content = get("layout__content")
+
+    object nav {
+
+      def get(className: String): String = {
+        mdl.get(s"navigation$className")
+      }
+
+      val container = get("")
+      val link = get("__link")
+
+    }
 
   }
 
